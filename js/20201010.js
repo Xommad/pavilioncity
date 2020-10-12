8df4b4cf81352b58a4dca4443ac8b94c5ab8cd3b
Xommad <almaflaty@gmail.com>
2020-10-12 22:14:03 +0200

    A big update

---
+++ js/20201010.js
@@ -0,0 +1,3 @@
+function DELETETHISshowThis() {
+    alert("I am his only one... He's lonely.. he has nothing... My father is lonely he... ohh man this makes me cry but why me? Why me? I always suck...My mother is depressed my Father is lonely and he has nobody to talk to and there is my brother... and don't really know him...")
+}
